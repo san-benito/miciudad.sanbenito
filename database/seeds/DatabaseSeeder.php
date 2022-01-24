e88a86d37019587d0c4525f0bdccc8c5ed0ec0ea
Franco Rosatto <francoe12012@gmail.com>
2022-01-24 10:02:02 -0300

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -4,16 +4,28 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {   
-        $this->call(SettingTableSeeder::class);
-        $this->call(FaqTableSeeder::class);
-        $this->call(RoleTableSeeder::class);
-        $this->call(DefaultDemoSeeder::class);
-    }
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $this->call(SettingTableSeeder::class);
+    $this->call(FaqTableSeeder::class);
+    $this->call(RoleTableSeeder::class);
+    //$this->call(DefaultDemoSeeder::class);
+
+    $admin = new User();
+    $admin->name = 'Admin';
+    $admin->surname = 'Participes';
+    $admin->email = 'admin@admin.com';
+    $admin->email_verified_at = now();
+    $admin->password = Hash::make('participes');
+    $admin->remember_token = Str::random(10);
+    $admin->save();
+    $admin->roles()->attach(Role::where('name', 'user')->first());
+    $admin->roles()->attach(Role::where('name', 'admin')->first());
+    $admin->save();
+  }
 }
